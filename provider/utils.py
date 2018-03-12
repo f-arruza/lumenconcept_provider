031bfbd2df9dcd49524afad6c0a50dc8ef755c6a
f-arruza <f.arruza@uniandes.edu.co>
2018-03-12 00:36:11 -0500

    region varible

--- provider/utils.py
+++ provider/utils.py
@@ -6,6 +6,7 @@ from django.conf import settings
 client = boto3.client('sqs',
                       aws_access_key_id = settings.AWS_ACCESS_KEY_ID,
                       aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY,
+                      region_name = settings.AWS_REGION,
                      )
 
 def send_message_sqs(queue, message):
